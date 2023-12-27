8b069fae2b87d44e7ff11af76183bf016ba0981a
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2023-12-26 22:34:36 -0300

    Entidade User

--- src/main/java/com/matheus/form/Entity/User.java
+++ src/main/java/com/matheus/form/Entity/User.java
@@ -2,12 +2,16 @@ package com.matheus.form.Entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
-@Table(name = "user")
+@Table(name = "user_table")
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
