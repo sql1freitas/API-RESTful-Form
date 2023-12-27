fcc6fc47502be0ff957747bb561885bf8bf76a5f
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2023-12-26 22:36:40 -0300

    Update FormApplication
    Add @ComponentScan

--- src/main/java/com/matheus/form/FormApplication.java
+++ src/main/java/com/matheus/form/FormApplication.java
@@ -2,7 +2,11 @@ package com.matheus.form;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@ComponentScan(basePackages = "com.matheus.form")
+@Configuration
 @SpringBootApplication
 public class FormApplication {
 
