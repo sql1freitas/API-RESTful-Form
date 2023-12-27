e1cf00436c9dde26832749592a6d178df5caa3c1
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2023-12-26 22:35:55 -0300

    Create UserRepository

---
+++ src/main/java/com/matheus/form/Repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.matheus.form.Repository;
+
+import com.matheus.form.Entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
